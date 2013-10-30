f91c96d0b9a81337e06ab39b4038308e46cf157d
Itay Shoshani <itay13@meet.mit.edu>
2013-10-30 19:24:15 +0200

    fixed the last number bug

--- Lab2/divisors.py
+++ Lab2/divisors.py
@@ -1,17 +1,16 @@
 def allNumbers():
 	n = int(raw_input("Please enter a number"))
-	for num in xrange(1,n):
+	for num in xrange(1,n + 1):
 		print num
 def divisorsOnly():
 	n = int(raw_input("Please enter a number"))
-	for num in xrange(1,n):
+	for num in xrange(1,n + 1):
 		if n % num == 0:
 			print num
 def divisors(n):
-	for num in xrange(1,n):
+	for num in xrange(1,n + 1):
 		if n % num == 0:
 			print num
 if __name__ == "__main__":
-	allNumbers()
 	divisorsOnly()
-	divisors(100)
+
